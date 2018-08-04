fcd391a9a7b126ac231be92b97c6fba4f86aba88
Joe (Hasee) <lsz2006.ad@gmail.com>
2018-08-05 00:38:03 +0100

    edit batch files and add results

---
+++ scrapy_tutorial/run_orderly.bat
@@ -0,0 +1,28 @@
+@echo off
+SET URL1="https://www.glassdoor.com/Interview/Google-Australia-Interview-Questions-EI_IE9079.0,6_IL.7,16_IN16.htm"
+SET URL2="https://www.glassdoor.com/Interview/Amazon-Australia-Interview-Questions-EI_IE6036.0,6_IL.7,16_IN16.htm"
+SET URL3="https://www.glassdoor.com/Interview/Facebook-Australia-Interview-Questions-EI_IE40772.0,8_IL.9,18_IN16.htm"
+SET URL4="https://www.glassdoor.com/Interview/Microsoft-Australia-Interview-Questions-EI_IE1651.0,9_IL.10,19_IN16.htm"
+SET URL5="https://www.glassdoor.com/Interview/Accenture-Australia-Interview-Questions-EI_IE4138.0,9_IL.10,19_IN16.htm"
+del /f temp.json
+@echo on
+
+scrapy crawl order -a url=%URL1% -o temp.json
+copy /y temp.json GOOG.json
+del /f temp.json
+
+scrapy crawl order -a url=%URL2% -o temp.json
+copy /y temp.json AMZN.json
+del /f temp.json
+
+scrapy crawl order -a url=%URL3% -o temp.json
+copy /y temp.json FB.json
+del /f temp.json
+
+scrapy crawl order -a url=%URL4% -o temp.json
+copy /y temp.json MSFT.json
+del /f temp.json
+
+scrapy crawl order -a url=%URL5% -o temp.json
+copy /y temp.json ACN.json
+del /f temp.json
--- scrapy_tutorial/run_unorderly.bat
+++ scrapy_tutorial/run_unorderly.bat
@@ -1,8 +1,9 @@
+@echo off
 SET URL1="https://www.glassdoor.com/Interview/Google-Australia-Interview-Questions-EI_IE9079.0,6_IL.7,16_IN16.htm"
-
 del /f temp.json
+@echo on
+
 scrapy crawl count -a url=%URL1%
 scrapy crawl fetch -o temp.json
-copy /y temp.json result_unordered.json
-
+copy /y temp.json GOOG_unordered.json
 del /f temp.json
