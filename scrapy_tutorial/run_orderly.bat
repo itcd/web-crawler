0c732ca06050f3873386bd3d1161f25e1aefed5a
Joe (Hasee) <lsz2006.ad@gmail.com>
2018-08-05 00:43:28 +0100

    edit batch files

--- scrapy_tutorial/run_orderly.bat
+++ scrapy_tutorial/run_orderly.bat
@@ -1,9 +1,9 @@
 @echo off
-SET URL1="https://www.glassdoor.com/Interview/Google-Australia-Interview-Questions-EI_IE9079.0,6_IL.7,16_IN16.htm"
-SET URL2="https://www.glassdoor.com/Interview/Amazon-Australia-Interview-Questions-EI_IE6036.0,6_IL.7,16_IN16.htm"
-SET URL3="https://www.glassdoor.com/Interview/Facebook-Australia-Interview-Questions-EI_IE40772.0,8_IL.9,18_IN16.htm"
-SET URL4="https://www.glassdoor.com/Interview/Microsoft-Australia-Interview-Questions-EI_IE1651.0,9_IL.10,19_IN16.htm"
-SET URL5="https://www.glassdoor.com/Interview/Accenture-Australia-Interview-Questions-EI_IE4138.0,9_IL.10,19_IN16.htm"
+set URL1="https://www.glassdoor.com/Interview/Google-Australia-Interview-Questions-EI_IE9079.0,6_IL.7,16_IN16.htm"
+set URL2="https://www.glassdoor.com/Interview/Amazon-Australia-Interview-Questions-EI_IE6036.0,6_IL.7,16_IN16.htm"
+set URL3="https://www.glassdoor.com/Interview/Facebook-Australia-Interview-Questions-EI_IE40772.0,8_IL.9,18_IN16.htm"
+set URL4="https://www.glassdoor.com/Interview/Microsoft-Australia-Interview-Questions-EI_IE1651.0,9_IL.10,19_IN16.htm"
+set URL5="https://www.glassdoor.com/Interview/Accenture-Australia-Interview-Questions-EI_IE4138.0,9_IL.10,19_IN16.htm"
 del /f temp.json
 @echo on
 
--- scrapy_tutorial/run_unorderly.bat
+++ scrapy_tutorial/run_unorderly.bat
@@ -1,5 +1,5 @@
 @echo off
-SET URL1="https://www.glassdoor.com/Interview/Google-Australia-Interview-Questions-EI_IE9079.0,6_IL.7,16_IN16.htm"
+set URL1="https://www.glassdoor.com/Interview/Google-Australia-Interview-Questions-EI_IE9079.0,6_IL.7,16_IN16.htm"
 del /f temp.json
 @echo on
 
